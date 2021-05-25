fc6c56bcf80fdf15be0c23a07465c61a3fd11bf5
yanliangzhao <79456556+yanliangzhao@users.noreply.github.com>
2021-05-25 19:53:33 +0800

    Add files via upload

--- step2.sh
+++ step2.sh
@@ -11,13 +11,13 @@ tCnt=`cat $cntFile`
 for ((i=1; i<=tCnt; i ++))
 do
 echo "对第$i个节点添加自动提取。"
-mkdir -p keys/node${i}
-cp /var/lib/bee/node${i}/keys/swarm.key ./keys/node${i}
 #mv ./keys/swarm.key ./keys/${ip}-${i}.key
 echo "00 02 * * * root /root/cashout${i}.sh cashout-all" >> /etc/crontab
 screen -dmS bee$i
 screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'
 echo "第$i个节点已启动。"
+mkdir -p keys/node${i}
+cp /var/lib/bee/node${i}/keys/swarm.key ./keys/node${i}
 screen -ls
 done
