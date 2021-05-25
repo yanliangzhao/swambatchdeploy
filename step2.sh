ff6ea45b340da08d60a5aee1107c4885e1a54e97
yanliangzhao <79456556+yanliangzhao@users.noreply.github.com>
2021-05-25 17:19:45 +0800

    Add files via upload

---
+++ step2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+mkdir -p keys
+cntFile=".showcnt.txt"
+if [ ! -f $cntFile ]; then
+echo "未运行step1！"
+exit
+fi
+
+tCnt=`cat $cntFile`
+for ((i=1; i<=tCnt; i ++))
+do
+echo "对第$i个节点添加自动提取。"
+mkdir -p keys/node${i}
+cp /var/lib/bee/node${i}/keys/swarm.key ./keys/node${i}
+#mv ./keys/swarm.key ./keys/${ip}-${i}.key
+echo "00 02 * * * root /root/cashout${i}.sh cashout-all" >> /etc/crontab
+screen -dmS bee$i
+screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
+screen -x -S bee$i -p 0 -X stuff $'\n'
+echo "第$i个节点已启动。"
+screen -ls
+done
---
+++ step3.sh
@@ -0,0 +1,24 @@
+tCnt=`cat .showcnt.txt`
+ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+password=`cat ./keys/${ip}-password.txt`
+#echo "${password}"
+wget https://github.com/ethersphere/exportSwarmKey/releases/download/v0.1.0/export-swarm-key-linux-386 && chmod 777 export-swarm-key-linux-386
+#echo "ip,num,dpi_port,address,chequebook,privatekey" > ${ip}.txt
+for ((i=1; i<=tCnt; i ++))
+do
+dpi_port=`cat node${i}.yaml | grep 'debug-api-addr: 127.0.0.1:' | awk -F ':' '{print $3}'`
+#echo "节点${i}的端口为：${dpi_port}"
+#echo "节点${i}的钱包地址和合约地址:"
+address=`curl -s localhost:${dpi_port}/addresses | jq .ethereum`
+#echo "address:${address}"
+chequebook=`curl -s http://localhost:${dpi_port}/chequebook/address | jq .chequebookAddress`
+#echo "chequebook:${chequebook}"
+key=`./export-swarm-key-linux-386 ./keys/node${i}/ ${password}`
+echo "${ip};${i};${dpi_port};${address};${chequebook};${key}"
+done
+#sz ${ip}.txt
+#echo "下载密钥至本地……"
+cp /var/lib/bee/password ./keys
+mv ./keys/password ./keys/${ip}-password.txt
+#sz ./keys/*
+
