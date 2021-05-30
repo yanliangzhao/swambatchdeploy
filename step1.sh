fd101bba1f62f604f700ec367f3cf30845bca0f0
颜良炤 <yanliangzhao@meituan.com>
2021-05-30 22:02:50 +0800

    func(service): bee

--- step1.sh
+++ step1.sh
@@ -9,13 +9,14 @@ sudo apt-get install -y lrzsz
 sudo apt-get install -y screen
 sudo apt-get install -y net-tools
 sudo apt-get install -y curl
-wget https://raw.githubusercontent.com/yanliangzhao/swambatchdeploy/main/bee053 && chmod 777 bee053
+wget https://github.com/ethersphere/bee/releases/download/v0.6.1/bee_0.6.1_amd64.deb
+#wget https://raw.githubusercontent.com/yanliangzhao/swambatchdeploy/main/bee053 && chmod 777 bee053
 #wget http://data.ming.cash/beeby/bee053 && chmod 777 bee053
 wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/b40510f1172b96c21d6d20558ca1e70d26d625c4/cashout.sh && chmod 777 cashout.sh
 wget https://raw.githubusercontent.com/yanliangzhao/swambatchdeploy/main/step2.sh && chmod 777 step2.sh
 wget https://raw.githubusercontent.com/yanliangzhao/swambatchdeploy/main/step3.sh && chmod 777 step3.sh
-#sudo dpkg -i bee_0.6.1_amd64.deb && sudo chown -R bee:bee /var/lib/bee
-mkdir -p /var/lib/bee && cd /var/lib/bee && touch password && echo "123456" > password && cd ~
+sudo dpkg -i bee_0.6.1_amd64.deb && sudo chown -R bee:bee /var/lib/bee
+#mkdir -p /var/lib/bee && cd /var/lib/bee && touch password && echo "123456" > password && cd ~
 echo "0" > $cntFile
 chmod +rw $cntFile
 sed -i 's/10000000000000000/1/g' cashout.sh
@@ -41,7 +42,7 @@ data-dir: /var/lib/bee/node${tCnt}
 cache-capacity: "2000000"
 block-time: "15"
 bootnode:
-- /ip4/134.195.197.62/tcp/1735/p2p/16Uiu2HAm5PSJmNBFiCJTYSyjjfeNERxQ6M2BjNbtjoRui4JpkwGU
+- /dnsaddr/bootnode.ethswarm.org
 debug-api-addr: 0.0.0.0:$((1634+${tCnt}))
 #debug-api-addr: 127.0.0.1:$((1634+${tCnt}))
 debug-api-enable: true
