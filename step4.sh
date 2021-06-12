2493dcad63abda09d16a43700bd18d676232e90a
颜良炤 <yanliangzhao@meituan.com>
2021-06-12 16:41:19 +0800

    step4

--- step4.sh
+++ step4.sh
@@ -4,13 +4,10 @@ epFile="epFile.txt"
 rm -rf cashou*
 wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/cashout.sh && chmod 777 cashout.sh
 sed -i 's/10000000000000000/1/g' cashout.sh
-ep=`cat $epFile`
 tCnt=`cat $cntFile`
-let tCnt++
-echo $tCnt > $cntFile
-cp cashout.sh cashout${tCnt}.sh
-sed -i "s/1635/$((1634+${tCnt}))/g" cashout${tCnt}.sh
-echo "cashout${tCnt}.sh文件已生成至当前目录"
-#bee start --config node${tCnt}.yaml
-
-
+for ((i=1; i<=tCnt; i ++))
+do
+cp cashout.sh cashout${i}.sh
+sed -i "s/1635/$((1634+${i}))/g" cashout${i}.sh
+echo "cashout${i}.sh文件已生成至当前目录"
+done
